0e01ce13c035684e7a3901a76cc431da1000e268
joel3500 <jsande@etu.uqac.ca>
2025-12-01 17:09:18 -0500

    Merge remote history

--- archives_python/test.py
+++
@@ -1,8 +0,0 @@
-noms = ['Alice', 'Bonnet', 'Coline', 'David']
-phrases = ['alice mange bien', 'rené a un bonnet', 'la coline est haute', 'david contre Goliath', 'suzanne et geremie']
-
-# Vérifier si chaque élément de la liste noms est présent dans au moins une phrase
-boolean = all(any(nom.lower() in phrase.lower() for phrase in phrases) for nom in noms)
-
-# Affichage du résultat
-print(f"Tous les noms sont présents : {boolean}")
